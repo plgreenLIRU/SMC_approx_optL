dc551e27a111662deb7b074b0e40b897ef1484af
Pete Green <p.l.green@liverpool.ac.uk>
2021-01-04 15:20:44 +0000

    Now plots estimated means

--- SDOF_example/2_SMC.py
+++ SDOF_example/2_SMC.py
@@ -13,7 +13,7 @@ from SMC_OPT import *
 # Load training data and model
 file_name = 'training_data.dat'
 file = open(file_name, 'rb')
-[x, y_true, y_obs, t, sdof, sigma] = dill.load(file)
+[F, y_true, y_obs, t, sdof, sigma] = dill.load(file)
 file.close()
 
 # Define prior
@@ -26,46 +26,47 @@ def q0_rvs(size):
     k = np.vstack(q0.p_k.rvs(size))
     c = np.vstack(q0.p_c.rvs(size))
     sigma = np.vstack(q0.p_sigma.rvs(size))
-    
+
     return np.hstack([k, c, sigma])
-    
+
 def q0_logpdf(x):
 
     # Convert to 2D array if currently 1D
     if len(np.shape(x))==1:
         x = np.array([x])
-    
+
     # Calculate logpdf
-    logpdf = (q0.p_k.logpdf(x[:, 0]) + 
-              q0.p_c.logpdf(x[:, 1]) + 
+    logpdf = (q0.p_k.logpdf(x[:, 0]) +
+              q0.p_c.logpdf(x[:, 1]) +
               q0.p_sigma.logpdf(x[:, 2]))
-              
+
     return logpdf
-    
+
 q0.rvs = q0_rvs
 q0.logpdf = q0_logpdf
 
-# Define L-kernel 
+# Define L-kernel
 L = L_Kernel()
-L.cov = 0.01 * np.array([[0.01, 0, 0], 
-                         [0, 0.001, 0], 
+L.cov = 0.01 * np.array([[0.01, 0, 0],
+                         [0, 0.001, 0],
                          [0, 0, 0.001]])
-L.inv_cov = np.linalg.inv(L.cov)                         
-L.logpdf = lambda x, x_cond : (-0.5 * (x - x_cond).T @ 
+L.inv_cov = np.linalg.inv(L.cov)
+L.logpdf = lambda x, x_cond : (-0.5 * (x - x_cond).T @
                                L.inv_cov @ (x - x_cond))
 
 # Define proposal
 q = Q_Proposal()
-q.cov = 0.01 * np.array([[0.01, 0, 0], 
-                         [0, 0.001, 0], 
+q.cov = 0.01 * np.array([[0.01, 0, 0],
+                         [0, 0.001, 0],
                          [0, 0, 0.001]])
-q.inv_cov = np.linalg.inv(q.cov)                         
-q.logpdf = lambda x, x_cond : (-0.5 * (x - x_cond).T @ 
+q.inv_cov = np.linalg.inv(q.cov)
+q.logpdf = lambda x, x_cond : (-0.5 * (x - x_cond).T @
                                q.inv_cov @ (x - x_cond))
-q.rvs = lambda x_cond : (x_cond + 
+q.rvs = lambda x_cond : (x_cond +
                          np.sqrt(np.diag(q.cov)) * np.random.randn(3))
+
 # Define log target distribution
-p = SDOF_Log_Posterior(x, y_obs, q0, sdof)
+p = SDOF_Log_Posterior(F, y_obs, q0, sdof)
 
 # No. samples and iterations
 N = 500
@@ -76,11 +77,25 @@ smc = SMC_BASE(N, 3, p, q0, K, q, L)
 smc.generate_samples()
 
 # SMC sampler with optimum L-kernel
-smc_optL = SMC_OPT(N=N, D=3, p=pi, q0=q0, K=K, q=q)
+smc_optL = SMC_OPT(N, 3, p, q0, K, q)
 smc_optL.generate_samples()
 
-# Save results
-file_name = 'SMC_results.dat'
-file = open(file_name, 'wb')
-dill.dump([smc, smc_optL], file)
-file.close()
+# Plot SMC results (estimates of means)
+fig, ax = plt.subplots(nrows=3)
+for i in range(3):
+    ax[i].plot(smc.mean_estimate_EES[:, i], 'k',
+               label='Forward proposal L-kernel')
+    ax[i].plot(smc_optL.mean_estimate_EES[:, i], 'r',
+               label='Optimal L-kernel')
+    ax[i].set_xlabel('Iteration')
+    if i == 0:
+        ax[i].set_ylabel('E[$k$]')
+    if i == 1:
+        ax[i].set_ylabel('E[$c$]')
+    if i == 2:
+        ax[i].set_ylabel('E[$\sigma$]')
+
+ax[0].legend(loc='upper left', bbox_to_anchor=(1, 1))
+plt.tight_layout()
+
+plt.show()
