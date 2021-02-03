d32217f9f188a24c5a44d701105dd3c5331c461b
Pete Green <p.l.green@liverpool.ac.uk>
2021-02-03 10:55:43 +0000

    Test now working in 4 dimensions

--- tests/test_SMC_OPT.py
+++ tests/test_SMC_OPT.py
@@ -12,33 +12,34 @@ P.L.Green and A.Lee
 """
 
 # Define target distribution
-p = Normal_PDF(mean=np.array([3.0, 2.0]), cov=np.eye(2))
+D = 4    # Dimension of problem
+p = Normal_PDF(mean=np.repeat(1, D), cov=np.eye(D))
 
 # Define initial proposal
-q0 = Normal_PDF(mean=np.zeros(2), cov=np.eye(2))
+q0 = Normal_PDF(mean=np.zeros(D), cov=np.eye(D))
 
 # Define proposal as being Gaussian, centered on x_cond, with identity
 # covariance matrix
 q = Q_Proposal()
 q.logpdf = lambda x, x_cond : -0.5 * (x - x_cond).T @ (x - x_cond)
-q.rvs = lambda x_cond : x_cond + np.random.randn(2)
+q.rvs = lambda x_cond : x_cond + np.random.randn(D)
 
 # Define L-kernel as being Gaussian, centered on x_cond, with identity
 # covariance matrix
 L = L_Kernel()
 L.logpdf = lambda x, x_cond : -0.5 * (x - x_cond).T @ (x - x_cond)
-L.rvs = lambda x_cond : x_cond + np.random.randn(2)
+L.rvs = lambda x_cond : x_cond + np.random.randn(D)
 
 # No. samples and iterations
 N = 1000
 K = 50
 
 # SMC sampler with user-defined L-kernel
-smc = SMC_BASE(N=N, D=2, p=p, q0=q0, K=K, q=q, L=L)
+smc = SMC_BASE(N=N, D=D, p=p, q0=q0, K=K, q=q, L=L)
 
 # SMC sampler with optimum L
-smc_opt = SMC_OPT(N=N, D=2, p=p, q0=q0, K=K, q=q)
-smc_opt_qr = SMC_OPT(N=N, D=2, p=p, q0=q0, K=K, q=q, QR_PCA = True, t = 1)
+smc_opt = SMC_OPT(N=N, D=D, p=p, q0=q0, K=K, q=q)
+smc_opt_qr = SMC_OPT(N=N, D=D, p=p, q0=q0, K=K, q=q, QR_PCA=True, t=D-2)
 
 def test_sampler():
     """ For this simple example, we test that the SMC estimates of target mean
